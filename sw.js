550a2943eb65ede5e6506da9426f44b9a9b8bc00
hedraa40-beep <hedraa40@gmail.com>
2026-05-31 01:42:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,21 +2,28 @@
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil((async () => {
     try {
       const keys = await caches.keys();
       await Promise.all(keys.map(k => caches.delete(k)));
     } catch (e) {}
+
     try {
       await self.registration.unregister();
     } catch (e) {}
+
     try {
-      const clientsList = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+      const clientsList = await self.clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true
+      });
       for (const client of clientsList) client.navigate(client.url);
     } catch (e) {}
   })());
 });
+
 self.addEventListener('fetch', event => {
   return;
 });
